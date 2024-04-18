aa0374c25b6cdc19555430bac344c98fdc444137
husada20 <husadahutasoit@gmail.com>
2024-04-18 23:37:45 +0700

    implemented api docs for users

--- src/Interfaces/http/api/users/routes.js
+++ src/Interfaces/http/api/users/routes.js
@@ -1,8 +1,33 @@
+const Joi = require('joi');
+
 const routes = (handler) => ([
   {
     method: 'POST',
     path: '/users',
     handler: handler.postUserHandler,
+
+    options: {
+      description: 'POST user',
+      notes: 'TEST',
+      tags: ['api', 'user'],
+      validate: {
+        payload: Joi.object({
+          fullname: Joi.string(),
+          username: Joi.string(),
+          password: Joi.string(),
+        }).label('POST-user-validate')
+      },
+      response: {
+        schema: Joi.object({
+          status: Joi.string().valid('success'),
+          data: Joi.object({
+            id: Joi.string(),
+            fullname: Joi.string(),
+            username: Joi.string(),
+          })
+        })
+      }
+    }
   },
 ]);
 
