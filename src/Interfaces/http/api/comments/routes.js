3244cfb47f57e2241e107801fe3bdb7114e3e3fb
husada20 <husadahutasoit@gmail.com>
2024-04-19 15:39:11 +0700

    implemented api docs with swagger API

--- src/Interfaces/http/api/comments/routes.js
+++ src/Interfaces/http/api/comments/routes.js
@@ -1,14 +1,74 @@
+const Joi = require("joi");
+
 const routes = (handler) => [
   {
     method: 'POST',
     path: '/threads/{threadId}/comments',
     handler: handler.postCommentHandler,
+
+    options: {
+      description: 'POST comments in thread',
+      notes: 'TEST',
+      tags: ['api', 'comment'],
+      validate: {
+        payload: Joi.object({
+          content: Joi.string()
+        }).label('Post-comment-validate'),
+        headers: Joi.object({
+          accessToken: Joi.string().required(),
+        })
+      },
+      response: {
+        schema: Joi.object({
+          status: Joi.string().valid('success'),
+          data: Joi.object({
+            addComments: {
+              id: Joi.string(),
+              content: Joi.string(),
+              owner: Joi.string(),
+            }
+          })
+        })
+      }
+    }
   },
+
   {
     method: 'DELETE',
     path: '/threads/{threadId}/comments/{commentId}',
     handler: handler.deleteCommentHandler,
+
+    options: {
+      description: 'DELETE comments from thread',
+      notes: 'TEST',
+      tags: ['api','comments'],
+      validate: {
+        payload: Joi.object({
+          threadId: Joi.string(),
+          commentId: Joi.string(),
+        }).label('DELETE-comment-validate'),
+        headers: Joi.object({
+          accessToken: Joi.string().required(),
+        }).unknown()
+      },
+      response: {
+        schema: Joi.object({
+          status: Joi.string().valid('success'),
+        })
+      }
+    }
   },
 ];
 
 module.exports = routes;
+
+// options: {
+//   description: 'DELETE comments in thread',
+//   notes: 'TEST',
+//   tags: ['api', 'comments'],
+//   validate: {
+//     payload: Joi.object({
+      
+//     })
+//   }
+// }
--- src/Interfaces/http/api/threads/routes.js
+++ src/Interfaces/http/api/threads/routes.js
@@ -1,13 +1,65 @@
+const Joi = require("joi");
+
 const routes = (handler) => [
   {
     method: 'POST',
     path: '/threads',
     handler: handler.postThreadHandler,
+
+    options: {
+      description: 'POST threads',
+      notes: 'TEST',
+      tags: ['api', 'thread'],
+      validate: {
+        payload: Joi.object({
+          title: Joi.string().required(),
+          body: Joi.string().required(),
+        }).label('POST-threads-validate'),
+        headers: Joi.object({
+          accessToken: Joi.string().required(),
+        })
+      },
+      response: {
+        schema: Joi.object({
+          status: Joi.string().valid('success'),
+          data: Joi.object({
+            addedThread: {
+              id: Joi.string(),
+              title: Joi.string(),
+              owner: Joi.string(),
+            }
+          })
+        })
+      }
+    }
   },
   {
     method: 'GET',
     path: '/threads/{threadId}',
     handler: handler.getDetailThreadHandler,
+
+    options: {
+      description: 'GET thread',
+      notes: 'TEST',
+      tags: ['api', 'thread'],
+      validate: {
+        params: Joi.object({
+          threadId: Joi.string().required()
+        }).label('GET-threads-validate'),
+        headers: Joi.object({
+          accessToken: Joi.string().required(),
+        })
+      },
+      response: {
+        schema: Joi.object({
+          status: Joi.string().valid('success'),
+          data: Joi.object({
+            title: Joi.string(),
+            owner: Joi.string(),
+          })
+        })
+      }
+    }
   },
 ];
 
